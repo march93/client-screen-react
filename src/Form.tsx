28be8a6f339d8d727cbc7bd233d1affbe9571027
Marc Ho <marc.ho380@gmail.com>
2023-12-27 19:06:49 -0800

    Add axios and search functionality

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import {
   CalendarMonth,
+  CancelRounded,
   CheckCircleRounded,
   FlagRounded,
   PersonRounded,
@@ -15,90 +16,101 @@ import {
   TextField,
   Typography,
 } from '@mui/material';
+import axios from 'axios';
+
+interface SearchResult {
+  name: boolean;
+  dob: boolean;
+  citizenship: boolean;
+}
 
 const formName = (
   name: string,
   setName: React.Dispatch<React.SetStateAction<string>>,
   nameMatched: boolean,
   nameError: boolean,
-  setNameError: React.Dispatch<React.SetStateAction<boolean>>
+  setNameError: React.Dispatch<React.SetStateAction<boolean>>,
+  searched: boolean,
+  setSearched: React.Dispatch<React.SetStateAction<boolean>>
 ) => {
   return (
-    <>
-      {/* Full Name */}
-      <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-        <PersonRounded sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-        <TextField
-          required
-          id="name-required"
-          label="Full Name"
-          variant="standard"
-          style={{ minWidth: '50%' }}
-          error={nameError}
-          value={name}
-          onChange={(e) => {
-            if (e.target.value.length > 0) {
-              setNameError(false);
-            }
+    <Box sx={{ display: 'flex', alignItems: 'flex-end', margin: '2em 0 0 0' }}>
+      <PersonRounded sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+      <TextField
+        required
+        id="name-required"
+        label="Full Name"
+        variant="standard"
+        style={{ minWidth: '50%' }}
+        error={nameError}
+        value={name}
+        onChange={(e) => {
+          if (e.target.value.length > 0) {
+            setNameError(false);
+          }
 
-            setName(e.target.value);
-          }}
-        />
-        <Box
-          sx={{
-            display: 'flex',
-            width: '100%',
-            justifyContent: 'flex-end',
-          }}
-        >
-          {nameMatched && <CheckCircleRounded />}
-        </Box>
+          setSearched(false);
+          setName(e.target.value);
+        }}
+      />
+      <Box
+        sx={{
+          display: 'flex',
+          width: '100%',
+          justifyContent: 'flex-end',
+        }}
+      >
+        {nameMatched && searched && <CheckCircleRounded />}
+        {!nameMatched && searched && <CancelRounded />}
       </Box>
-    </>
+    </Box>
   );
 };
 
 const formBirth = (
-  birthYear: number | undefined,
+  birthyear: number | undefined,
   setBirthYear: React.Dispatch<React.SetStateAction<number | undefined>>,
   birthYearMatched: boolean,
   birthError: boolean,
-  setBirthError: React.Dispatch<React.SetStateAction<boolean>>
+  setBirthError: React.Dispatch<React.SetStateAction<boolean>>,
+  searched: boolean,
+  setSearched: React.Dispatch<React.SetStateAction<boolean>>
 ) => {
   return (
-    <>
-      {/* Birth Year */}
-      <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-        <CalendarMonth sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-        <TextField
-          required
-          id="year-required"
-          label="Birth Year"
-          type="number"
-          variant="standard"
-          inputProps={{ min: 0, max: new Date().getFullYear() }}
-          style={{ minWidth: '50%' }}
-          error={birthError}
-          value={birthYear}
-          onChange={(e) => {
-            if (e.target.value !== undefined) {
-              setBirthError(false);
-            }
+    <Box
+      sx={{ display: 'flex', alignItems: 'flex-end', margin: '1em 0 1em 0' }}
+    >
+      <CalendarMonth sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+      <TextField
+        required
+        id="year-required"
+        label="Birth Year"
+        type="number"
+        variant="standard"
+        inputProps={{ min: 0, max: new Date().getFullYear() }}
+        style={{ minWidth: '50%' }}
+        error={birthError}
+        value={birthyear}
+        onChange={(e) => {
+          if (!!!e.target.value) {
+            setBirthError(false);
+          }
 
-            setBirthYear(parseInt(e.target.value));
-          }}
-        />
-        <Box
-          sx={{
-            display: 'flex',
-            width: '100%',
-            justifyContent: 'flex-end',
-          }}
-        >
-          {birthYearMatched && <CheckCircleRounded />}
-        </Box>
+          setSearched(false);
+          setBirthYear(parseInt(e.target.value));
+        }}
+      />
+      <Box
+        sx={{
+          display: 'flex',
+          width: '100%',
+          justifyContent: 'flex-end',
+        }}
+      >
+        {birthYearMatched && searched && <CheckCircleRounded />}
+        {!birthYearMatched && searched && <CancelRounded />}
       </Box>
-    </>
+    </Box>
   );
 };
 
@@ -107,56 +119,85 @@ const formCountry = (
   setCountry: React.Dispatch<React.SetStateAction<string>>,
   countryMatched: boolean,
   countryError: boolean,
-  setCountryError: React.Dispatch<React.SetStateAction<boolean>>
+  setCountryError: React.Dispatch<React.SetStateAction<boolean>>,
+  searched: boolean,
+  setSearched: React.Dispatch<React.SetStateAction<boolean>>
 ) => {
   return (
-    <>
-      {/* Country */}
-      <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-        <FlagRounded sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-        <TextField
-          required
-          id="country-required"
-          label="Country"
-          variant="standard"
-          style={{ minWidth: '50%' }}
-          error={countryError}
-          value={country}
-          onChange={(e) => {
-            if (e.target.value.length > 0) {
-              setCountryError(false);
-            }
+    <Box sx={{ display: 'flex', alignItems: 'flex-end', margin: '0 0 1em 0' }}>
+      <FlagRounded sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+      <TextField
+        required
+        id="country-required"
+        label="Country"
+        variant="standard"
+        style={{ minWidth: '50%' }}
+        error={countryError}
+        value={country}
+        onChange={(e) => {
+          if (e.target.value.length > 0) {
+            setCountryError(false);
+          }
 
-            setCountry(e.target.value);
-          }}
-        />
-        <Box
-          sx={{
-            display: 'flex',
-            width: '100%',
-            justifyContent: 'flex-end',
-          }}
-        >
-          {countryMatched && <CheckCircleRounded />}
-        </Box>
+          setSearched(false);
+          setCountry(e.target.value);
+        }}
+      />
+      <Box
+        sx={{
+          display: 'flex',
+          width: '100%',
+          justifyContent: 'flex-end',
+        }}
+      >
+        {countryMatched && searched && <CheckCircleRounded />}
+        {!countryMatched && searched && <CancelRounded />}
       </Box>
-    </>
+    </Box>
   );
 };
 
 function Form() {
   const [name, setName] = React.useState('');
-  const [birthYear, setBirthYear] = React.useState<number | undefined>();
+  const [birthyear, setBirthYear] = React.useState<number | undefined>();
   const [country, setCountry] = React.useState<string>('');
 
   const [nameMatched, setNameMatched] = React.useState(false);
   const [birthYearMatched, setBirthYearMatched] = React.useState(false);
   const [countryMatched, setCountryMatched] = React.useState(false);
+  const [searched, setSearched] = React.useState(false);
 
   // Error States
   const [nameError, setNameError] = React.useState(false);
   const [birthError, setBirthError] = React.useState(false);
   const [countryError, setCountryError] = React.useState(false);
+
+  const search = async () => {
+    setSearched(true);
+
+    if (!name || !!!birthyear || !country) {
+      // Set error states if any are empty
+      setNameError(!!!name);
+      setBirthError(!!!birthyear);
+      setCountryError(!!!country);
+      return;
+    }
+
+    const request = await axios.post(
+      `${process.env.REACT_APP_BACKEND_URL}/search`,
+      {
+        name,
+        birthyear,
+        country,
+      }
+    );
+    const data: SearchResult = request.data;
+
+    // Update matcher states
+    setNameMatched(data.name);
+    setBirthYearMatched(data.dob);
+    setCountryMatched(data.citizenship);
+  };
 
   return (
     <Grid
@@ -175,24 +216,36 @@ function Form() {
             OFAC User Search
           </Typography>
 
-          {formName(name, setName, nameMatched, nameError, setNameError)}
+          {formName(
+            name,
+            setName,
+            nameMatched,
+            nameError,
+            setNameError,
+            searched,
+            setSearched
+          )}
           {formBirth(
-            birthYear,
+            birthyear,
             setBirthYear,
             birthYearMatched,
             birthError,
-            setBirthError
+            setBirthError,
+            searched,
+            setSearched
           )}
           {formCountry(
             country,
             setCountry,
             countryMatched,
             countryError,
-            setCountryError
+            setCountryError,
+            searched,
+            setSearched
           )}
         </CardContent>
         <CardActions style={{ justifyContent: 'flex-end' }}>
-          <Button size="medium" variant="contained">
+          <Button size="medium" variant="contained" onClick={search}>
             Submit
           </Button>
         </CardActions>
